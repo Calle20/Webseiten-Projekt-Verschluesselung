15284202d8dc5de315ca9d452b96644c177bdb85
Calle20 <97348165+Calle20@users.noreply.github.com>
2022-01-24 18:31:30 +0100

    encoding löscht sich aber kein neues

--- public/Base64Animation.js
+++ public/Base64Animation.js
@@ -1,9 +1,11 @@
+var splitarrayout=[]
 function Base64_Decode(){
     for (let index = 0; index < 2; index++) {
         var input=document.getElementsByTagName("p")[index+13]
         var inputtxt=input.innerHTML
         var output=btoa(inputtxt)
-        input.innerHTML=output;
+        splitarrayout=output.split(" ")
+        input.innerHTML=" "+output;
     }
     var input=document.getElementsByTagName("h1")[0]
     var inputtxt=input.innerHTML
@@ -15,12 +17,26 @@ function Base64_Decode(){
     input.innerHTML=output;
     setTimeout(message,150)
 }
-function Base64_Encode(){
+async function Base64_Encode(){
     for (let index = 0; index < 2; index++) {
         var input=document.getElementsByTagName("p")[index+13]
         var inputtxt=input.innerHTML
         var output=atob(inputtxt)
-        input.innerHTML=output;
+        var splitarrayout=output.split(" ")
+        var del=inputtxt.length/splitarrayout.length
+        console.log(splitarrayout.length)
+        console.log(inputtxt.length)
+        var encodedstrnum=0
+        console.log(del)
+        for (let i = 0; i < splitarrayout.length; i++) {
+            var out=splitarrayout[i]
+            encodedstrnum=encodedstrnum+out.length+1
+            var txtin=input.innerHTML
+            var txtin2=txtin.slice(-txtin.length+Math.round(del)) 
+            var inputencoded=input.innerHTML.slice(0,encodedstrnum)
+            input.innerHTML=txtin2
+            await delay(200)
+        }
     }
     var input=document.getElementsByTagName("h1")[0]
     var inputtxt=input.innerHTML
@@ -33,5 +49,10 @@ function Base64_Encode(){
 }
 function message(){
     alert("Das ist der Text dieser Webseite mit Base64 verschlüsselt. ")
-    setTimeout(Base64_Encode,2000)
+    setTimeout(Base64_Encode,1500)
+}
+function delay(milliseconds){
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
 }
