409d018bf8fceaa7aa773b68dc83681f9457b26a
Calle20 <97348165+Calle20@users.noreply.github.com>
2022-01-25 17:51:21 +0100

    Animation Base 64 funktioniert

--- public/Base64Animation.js
+++ public/Base64Animation.js
@@ -1,10 +1,8 @@
-var splitarrayout=[]
 function Base64_Decode(){
     for (let index = 0; index < 2; index++) {
         var input=document.getElementsByTagName("p")[index+13]
         var inputtxt=input.innerHTML
         var output=btoa(inputtxt)
-        splitarrayout=output.split(" ")
         input.innerHTML=" "+output;
     }
     var input=document.getElementsByTagName("h1")[0]
@@ -18,34 +16,66 @@ function Base64_Decode(){
     setTimeout(message,150)
 }
 async function Base64_Encode(){
+    var input=document.getElementsByTagName("h1")[0]
+    var inputtxt=input.innerHTML
+    var output=atob(inputtxt)
+    var splitarrayout=output.split(" ")
+    var del=inputtxt.length/splitarrayout.length
+    var out=""
+    for (let i = 0; i < splitarrayout.length; i++) {
+        out=out+splitarrayout[i]+" "
+        var slicer=-inputtxt.length+((i+1)*Math.round(del))
+        var txtin2=inputtxt.slice(slicer) 
+        input.innerHTML=out+txtin2
+        if(i==splitarrayout.length-1){
+            var newtxtin2=input.innerHTML
+            var newsplitarrayout=newtxtin2.split(" ")
+            newsplitarrayout.pop()
+            input.innerHTML=newsplitarrayout.join(" ")
+        }
+        await delay(50)
+    }
+    var input=document.getElementsByTagName("h2")[0]
+    var inputtxt=input.innerHTML
+    var output=atob(inputtxt)
+    var splitarrayout=output.split(" ")
+    var del=inputtxt.length/splitarrayout.length
+    var out=""
+    for (let i = 0; i < splitarrayout.length; i++) {
+        out=out+splitarrayout[i]+" "
+        var slicer=-inputtxt.length+((i+1)*Math.round(del))
+        var txtin2=inputtxt.slice(slicer) 
+        input.innerHTML=out+txtin2
+        if(i==splitarrayout.length-1){
+            var newtxtin2=input.innerHTML
+            var newsplitarrayout=newtxtin2.split(" ")
+            newsplitarrayout.pop()
+            input.innerHTML=newsplitarrayout.join(" ")
+        }
+        await delay(50)
+    }
     for (let index = 0; index < 2; index++) {
         var input=document.getElementsByTagName("p")[index+13]
         var inputtxt=input.innerHTML
         var output=atob(inputtxt)
         var splitarrayout=output.split(" ")
         var del=inputtxt.length/splitarrayout.length
-        console.log(splitarrayout.length)
-        console.log(inputtxt.length)
-        var encodedstrnum=0
-        console.log(del)
+        var out=""
         for (let i = 0; i < splitarrayout.length; i++) {
-            var out=splitarrayout[i]
-            encodedstrnum=encodedstrnum+out.length+1
-            var txtin=input.innerHTML
-            var txtin2=txtin.slice(-txtin.length+Math.round(del)) 
-            var inputencoded=input.innerHTML.slice(0,encodedstrnum)
-            input.innerHTML=txtin2
-            await delay(200)
+            out=out+splitarrayout[i]+" "
+            var slicer=-inputtxt.length+((i+1)*Math.round(del))
+            var txtin2=inputtxt.slice(slicer) 
+            input.innerHTML=out+txtin2
+            if(i==splitarrayout.length-1){
+                var newtxtin2=input.innerHTML
+                var newsplitarrayout=newtxtin2.split(" ")
+                newsplitarrayout.pop()
+                input.innerHTML=newsplitarrayout.join(" ")
+            }
+            await delay(50)
         }
     }
-    var input=document.getElementsByTagName("h1")[0]
-    var inputtxt=input.innerHTML
-    var output=atob(inputtxt)
-    input.innerHTML=output;
-    var input=document.getElementsByTagName("h2")[0]
-    var inputtxt=input.innerHTML
-    var output=atob(inputtxt)
-    input.innerHTML=output;
+    
 }
 function message(){
     alert("Das ist der Text dieser Webseite mit Base64 verschlüsselt. ")
